0b61cdb3b82ea3da7c080e33dca25c0b36f6676b
Shinozaki Robyn Natsuko <shinozakirobynnatsuko@Shinozakis-MacBook-Air.local>
2016-08-14 08:56:56 +0545

    add in create method

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -4,5 +4,18 @@ class PlacesController < ApplicationController
 		@places=Place.all
 	end 	
 
+	def new
+		@place=Place.new 
+	end
 
+	def create
+		@place=Place.create(place_params)
+		redirect_to root_path
+	end
+
+private
+
+def place_params
+	params.require(:place).permit(:name, :adress, :description)
+end
 end
