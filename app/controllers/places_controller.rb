c54ffdb59c5603cb0f3c9578ca323662a98cde07
Shinozaki Robyn Natsuko <shinozakirobynnatsuko@Shinozakis-MacBook-Air.local>
2016-08-15 11:02:47 +0545

    validations

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,5 +1,5 @@
 class PlacesController < ApplicationController
-	before_action :authenticate_user!, only: [:new, :create, :edit, :update]
+	before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
 	def index
 		@places=Place.all
 	end 	
@@ -9,8 +9,13 @@ class PlacesController < ApplicationController
 	end
 
 	def create
-		current_user.places.create(place_params)		
-		redirect_to root_path
+		current_user.places.create(place_params)
+		if @place.valid?		
+			redirect_to root_path
+		else
+			render :new, status: :unprocessable_entity 
+		end
+
 	end
 
 	def show
@@ -19,24 +24,31 @@ class PlacesController < ApplicationController
 
 	def edit
 		@place=Place.find(params[:id])
-		if 
-			@place.user != current_user
+		if @place.user != current_user
 			return render text: "Not Allowed!", status: :forbidden
 		end
 	end
 
 	def update
 		@place=Place.find(params[:id])
-		if 
-			@place.user != current_user 
+		if @place.user != current_user 
 			return render text: "Not Allowed!", status: :forbidden
 		end 
-			@place.update_attributes(place_params)
+		@place.update_attributes(place_params)
+		if @place.valid?
 			redirect_to root_path
+		else 
+			render :edit, status: :unprocessable_entity 
+		end
+
 	end
+
 
 	def destroy 
 		@place=Place.find(params[:id])
+		if @place.user != current_user 
+			return render text: "Not Allowed!", status: :forbidden
+		end
 		@place.destroy
 		redirect_to root_path
 	end
--- app/models/place.rb
+++ app/models/place.rb
@@ -1,3 +1,5 @@
 class Place < ActiveRecord::Base
 	belongs_to :user
+	validates :name, :address, :description, presence: true
+	validates :description, length: {minimum: 3} 
 end
